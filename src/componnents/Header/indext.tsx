22b52961c9462c62a891109483cba8c2b77ade62
Petro Shevchyshen <shevchyshen@gmail.com>
2026-05-07 22:58:39 +0300

    fixed header z-index

--- src/componnents/Header/indext.tsx
+++ src/componnents/Header/indext.tsx
@@ -2,7 +2,7 @@ import { Navigation } from "../Navigation";
 
 export const Header = () => {
   return (
-    <div className="flex justify-around items-center sticky top-0 h-16 bg-white opacity-85">
+    <div className="flex justify-around items-center sticky top-0 h-16 bg-white opacity-85 z-50">
       <p>tend</p>
       <Navigation />
       <p>en</p>
